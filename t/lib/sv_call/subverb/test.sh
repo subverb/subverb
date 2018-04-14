1de32d2013e5a8c65ff69933d1511973a9ed7f6e
Benjamin Tietz <benjamin@micronet24.de>
2018-04-14 20:39:19 +0200

    [subverb] compute base-pathes dynamically
    * makes subverb callable from everywhere
    * get rid of default 'usr/' item in path
    * easier testing for real apps

--- t/lib/sv_call/subverb/test.sh
+++ t/lib/sv_call/subverb/test.sh
@@ -1,5 +1,6 @@
-bdir=usr/lib/sv_call/
-for i in found shell error nf "nf found" nf "nf found error"; do
+bdir=lib/sv_call/
+for i in found shell error nf "nf found" "nf found error"; do
+	RVAL="inv"
 	echo -n "$i: "
 	items=
 	for f in $i; do
