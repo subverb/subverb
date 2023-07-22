f51a926c8144d553044983977d79a61617481043
Benjamin Tietz <benjamin@micronet24.de>
2023-07-22 09:58:12 +0200

    [test] support input on commands

--- t/common.mk
+++ t/common.mk
@@ -1,6 +1,9 @@
 export OUTPUT
 export DESCRIPTION
 export SVBASEDIR=.
+ifdef INPUT
+export INPUT
+endif
 
 V?=@
 LN_SF?=ln -sf
@@ -21,7 +24,12 @@ $(COMMAND): $(TOPDIR)/subverb
 ifndef TEST_OWN_CMDDEF
 .$(COMMAND).res:
 	@echo ./$(COMMAND) $(GARGUMENTS) $(SUBVERB) $(ARGUMENTS)
+ifdef INPUT
+	$(V)echo "$$INPUT" > .$(COMMAND).input
+	$(V)./$(COMMAND) $(GARGUMENTS) $(SUBVERB) $(ARGUMENTS) < .$(COMMAND).input | tee .$(COMMAND).res
+else
 	$(V)./$(COMMAND) $(GARGUMENTS) $(SUBVERB) $(ARGUMENTS) | tee .$(COMMAND).res
+endif
 endif
 
 .$(COMMAND).ref: $(COMMAND).tmk common.mk | banner
